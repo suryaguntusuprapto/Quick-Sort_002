e2babd6b7534a6e71c0326fe4664908fed49f381
suryaguntursuprapto <suryaguntur2000@gmail.com>
2023-03-30 11:39:24 +0700

    quicksort_002

--- Quick Sort/Quick Sort.cpp
+++ Quick Sort/Quick Sort.cpp
@@ -93,4 +93,14 @@ void q_short(int low, int high) {
 }
 
 
+void display() {
+	cout << "\n------------------" << endl;
+	cout << "Sorted Array" << endl;
+	cout << "-----------------" << endl;
 
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << "\n\nNumber of comparisions: " << cmp_count << endl;
+	cout << " Number of data movements: " << mov_count << endl;
+}
