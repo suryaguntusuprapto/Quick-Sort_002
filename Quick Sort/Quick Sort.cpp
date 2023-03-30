5da0a52fee65b05d5c8fbcb3f85ed528347cea5c
suryaguntursuprapto <suryaguntur2000@gmail.com>
2023-03-30 11:25:40 +0700

    quicksort_002

--- Quick Sort/Quick Sort.cpp
+++ Quick Sort/Quick Sort.cpp
@@ -36,3 +36,44 @@ void swap(int x, int y) {
 	arr[x] = arr[y];
 	arr[y] = temp;
 }
+
+void q_short(int low, int high) {
+	int pivot, i, j;
+	if (low > high) {             //langkah 1
+		return;
+	}
+
+	//partition the list into two parts
+	//one containing elements less that or equal to pivot
+	//outher containing elements greather than pivot 
+
+	pivot = arr[low];            //langkah 2
+
+	i = low + 1;                 //langkah 3
+	j = high;                    //langkah 4
+
+
+	while (i <= j) {             //langkah 10
+		//search for an element greater than pivot 
+		while ((arr[i] <= pivot) && (i <= high)) {     //langkah 5
+			i++;                                       //langkah 6
+			cmp_count++;
+		}
+		cmp_count++;
+	}
+	cmp_count++;
+	//search for an element less than or equal to pivot
+	while ((arr[j] > pivot) && (j >= low)) {             //langkah 7
+		j--;                                             //langkah 8
+		cmp_count++;
+	}
+	cmp_count++;
+
+	//langkah 9
+	if (i < j) {    //if the greater element is on the left of the element
+		//swap the element at index i with the element at index j
+		swap(i, j);
+		mov_count++;
+	}
+}
+
